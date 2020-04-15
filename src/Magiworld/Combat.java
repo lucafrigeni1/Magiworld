6d7cec669adb8163932e2283d244e5e33adff62b
lucafrigeni1 <jean.frigeni@gmail.com>
2020-04-15 14:12:54 +0200

    exceptions ajouté dans Combat (mais ne fonctionne pas correctement)

--- src/Magiworld/Combat.java
+++ src/Magiworld/Combat.java
@@ -1,6 +1,7 @@
 package Magiworld;
 
 import Magiworld.Heros.Heros;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,12 +9,11 @@ public class Combat {
 
     static Scanner sc = new Scanner(System.in);
 
-    public static void start(Heros joueur1, Heros joueur2){
+    public static void start(Heros joueur1, Heros joueur2) {
 
         int attaque = -1;
 
-        while (joueur1.getVie() > 0 && joueur2.getVie() > 0)
-        {
+        while (joueur1.getVie() > 0 && joueur2.getVie() > 0) {
             do {
                 System.out.println("J1 à ton tour");
                 System.out.println("1. Attaque basique ");
@@ -21,29 +21,30 @@ public class Combat {
 
                 try {
                     attaque = sc.nextInt();
-                }catch (InputMismatchException c){
+                } catch (InputMismatchException c) {
                     sc.nextLine();
                 }
 
                 if (attaque != 1 && attaque != 2)
                     System.out.println("saisissez 1 ou 2");
 
-            }while (attaque != 1 && attaque != 2);
+            } while (attaque != 1 && attaque != 2);
 
-            if (attaque == 1){
+            if (attaque == 1) {
                 joueur1.aBasique(joueur2);
                 System.out.println("J2 possède " + joueur2.getVie() + " points de vie");
                 System.out.println("J1 possède " + joueur1.getVie() + " points de vie");
                 System.out.println();
-            }
-            else {
+            } else {
                 joueur1.aSpécial(joueur2);
                 System.out.println("J2 possède " + joueur2.getVie() + " points de vie");
                 System.out.println("J1 possède " + joueur1.getVie() + " points de vie");
                 System.out.println();
             }
 
-            if (joueur2.getVie() <= 0 ) {
+            attaque = 0;
+
+            if (joueur2.getVie() <= 0) {
                 System.out.println("Le Joueur 2 a perdu");
                 break;
             }
@@ -53,32 +54,33 @@ public class Combat {
                 System.out.println("1. Attaque basique ");
                 System.out.println("2. Attaque spécial ");
 
-            try {
-                attaque = sc.nextInt();
-            }catch (InputMismatchException b){
-                sc.nextLine();
-            }
+                try {
+                    attaque = sc.nextInt();
+                } catch (InputMismatchException b) {
+                    sc.nextLine();
+                }
 
 
-            if (attaque != 1 && attaque != 2)
-                System.out.println("saisissez 1 ou 2");
+                if (attaque != 1 && attaque != 2)
+                    System.out.println("saisissez 1 ou 2");
 
             } while (attaque != 1 && attaque != 2);
 
-            if (attaque == 1){
+            if (attaque == 1) {
                 joueur2.aBasique(joueur1);
                 System.out.println("J1 possède " + joueur1.getVie() + " points de vie");
                 System.out.println("J2 possède " + joueur2.getVie() + " points de vie");
                 System.out.println();
-            }
-            else {
+            } else {
                 joueur2.aSpécial(joueur1);
                 System.out.println("J1 possède " + joueur1.getVie() + " points de vie");
                 System.out.println("J2 possède " + joueur2.getVie() + " points de vie");
                 System.out.println();
             }
 
-            if (joueur1.getVie() < 0 ) {
+            attaque = 0;
+
+            if (joueur1.getVie() < 0) {
                 System.out.println("Le joueur 1 a perdu");
             }
         }
--- src/Magiworld/Main.java
+++ src/Magiworld/Main.java
@@ -5,4 +5,3 @@ public class Main {
         Game.start();
     }
 }
-1
